137c546ae3b285c906c11628011ca42904d6c3df
Ricky <henryats@gmail.com>
2023-09-15 16:05:56 +0100

    🔥Random Makefile Commit🔥

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,1 +1,24 @@
-mysql_secure_installation
+#!/bin/sh
+mysql_install_db
+/etc/init.d/mysql start
+if [ -d "/var/lib/mysql/$MYSQL_DB"]
+then
+    echo "Database already exist"
+else
+service mariadb start
+mysql_secure_installation << _EOF_
+Y
+$MYSQL_ROOT_PASS
+$MYSQL_ROOT_PASS
+Y
+n
+Y
+Y
+_EOF_
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB; GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
+sleep 1
+service mariadb stop
+fi
+
+exec "$@"
