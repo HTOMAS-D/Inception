32c35afa9a39fdbb97d4e2821adca58c9110a14a
Ricky <henryats@gmail.com>
2023-09-20 11:48:39 +0100

    🔥Random Makefile Commit🔥

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -15,3 +15,4 @@ sed -i "s/username_here/$DB_USER/g" ./wordpress/wp-config-sample.php
 sed -i "s/password_here/$DB_PASSWORD/g" ./wordpress/wp-config-sample.php
 sed -i "s/localhost/$DB_HOST/g" ./wordpress/wp-config-sample.php
 sed -i "s/database_name_here/$DB_NAME/g" ./wordpress/wp-config-sample.php
+cp /wordpress/wp-config-sample.php /wordpress/wp-config.php
